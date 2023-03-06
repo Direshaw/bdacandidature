9d264c56ed121f6b18553ad33902b2715968f85a
Direshaw <baba.legros@mail.com>
2023-03-06 12:40:14 +0000

    premier commit

---
+++ javascript/main.js
@@ -0,0 +1,10 @@
+const audio = new Audio();
+audio.src= "audio/SpongeBob theme song Earrape.mp3"
+
+function change() {
+	document.body.style.background="url(/Images/65a6b0d42f2652a01e0ac62f4d67309a.jpg)";
+    document.body.style.backgroundRepeat="no-repeat";
+    document.body.style.height="100%";
+    document.body.style.backgroundSize= "cover";
+
+}
